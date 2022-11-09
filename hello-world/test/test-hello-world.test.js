28b00eba72a09cfb6336ad8cc5ea9302b589472e
nodeFade <fadelb@axis.com>
2022-11-09 10:16:07 +0100

    add the test hello world javascript file

---
+++ hello-world/test/test-hello-world.test.js
@@ -0,0 +1,23 @@
+const {grabEl} = require('../helpers.js');
+
+// function getElementsById(ids) {
+//     var idList = ids.split(" ");
+//     var results = [], item;
+//     for (var i = 0; i < idList.length; i++) {
+//         item = document.getElementById(idList[i]);
+//         if (item) {
+//             results.push(item);
+//         }
+//     }
+//     return(results);
+// }
+// doStuff(getElementsById("myCircle1 myCircle2 myCircle3 myCircle4"));
+
+
+describe('Test the uppload of hello-world application on the cameras', () => {
+
+    test('Search in the interface if there is hello-world application', () =>{
+        let content = grabEl.getElementsByClassName("Apps__AppsContent-sc-3791tt-0.edSaLB");
+        expect(content).toContain('hello_world')
+    })
+})
